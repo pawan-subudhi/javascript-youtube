bd1eb9b6dc81b7ea0baead017d897a82769d9d1f
Pawan Kumar <37031863+pawan-subudhi@users.noreply.github.com>
2024-01-02 11:56:15 +0000

    more on data type conversion

--- 01_basics/03_datatype_conversion.js
+++ 01_basics/03_datatype_conversion.js
@@ -31,3 +31,39 @@ console.table([is_loggedin, typeof is_loggedin,boolean_is_loggedin, typeof boole
  *  "" => false
  *  "pawan" => true
  */
+
+// *************************************** OPERATIONS *****************************************************
+
+let value = 3
+let negative_value = -value
+
+console.log(negative_value);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) // power operation
+// console.log(2/2)
+// console.log(2%2)
+
+
+let str1 = "hello"
+let str2 = "Pawan"
+
+// console.log(str1 + str2);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
